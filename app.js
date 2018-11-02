c093d8c1c05e84c683a7a2a63be9e467a901a5bc
tongtong <1053673440@qq.com>
2018-11-02 11:55:31 +0800

    修复了css的问题，完成了订单详情页面

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require('http-errors')
+    ,express = require('express')
+    ,path = require('path')
+    ,cookieParser = require('cookie-parser')
+    ,logger = require('morgan')
+    ,indexRouter = require('./routes/index')
+    ,usersRouter = require('./routes/users')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
